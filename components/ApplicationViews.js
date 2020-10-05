9410d6b59831822dbf0d033815ef78d21d069664
joseph.tidwell09@gmail.com <joseph.tidwell09@gmail.com>
2020-10-05 14:58:01 -0500

    updated using hooks to display a list of dogs

--- components/ApplicationViews.js
+++ components/ApplicationViews.js
@@ -2,6 +2,9 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { Home } from "./Home"
 import { AnimalCard } from "./animal/AnimalCard"
+import { AnimalProvider } from "./animal/AnimalProvider"
+import { AnimalList } from "./animal/AnimalList"
+
 
 export const ApplicationViews = (props) => {
     return (
@@ -11,10 +14,13 @@ export const ApplicationViews = (props) => {
                 <Home />
             </Route>
 
+
             {/* Render the animal list when http://localhost:3000/animals */}
-            <Route path="/animals">
-                <AnimalCard />
-            </Route>
+            <AnimalProvider>
+                <Route exact path="/animals">
+                    <AnimalList />
+                </Route>
+            </AnimalProvider>
         </>
     )
 }
